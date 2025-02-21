89c3417d3ff9d8e646182b1f8fb50c5fd95623ae
Jijo Jacob <Jijojacob988@gmail.com>
2025-02-21 10:20:59 +0530

    Fixed base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-Jijo/',  // ✅ Ensure correct base for GitHub Pages
-  assetsInclude: ['**/*.png', '**/*.PNG'],  // ✅ Fix PNG Import Issue
+  base: '/',  // ✅ Set base to '/' for Vercel
+  assetsInclude: ['**/*.png', '**/*.PNG'], // ✅ Fix PNG Import Issue
 });
